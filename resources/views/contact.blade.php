115ff6b87f25045ceca97dda3630f29280d13d98
Pramartha <pramarthamahottama@gmail.com>
2025-01-14 00:03:43 +0800

    firat commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Ini Contact Saya</h1>
+
+    <ul>
+        <li><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
+        <li><a href="https://www.facebook.com" target="_blank">facebook</a></li>
+        <li><a href="https://www.tiktok.com" target="_blank">tiktok</a></li>
+        <li><a href="https://id.linkedin.com" target="_blank">linkedin</a></li>
+        <li><a href="https://www.youtube.com" target="_blank">youtube</a></li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Pramartha']);
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
